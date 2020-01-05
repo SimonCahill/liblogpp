b98d0738dfe9839a717df73e183a783a0d7b0758
Simon Cahill <simon@h3lix.de>
2020-01-05 13:42:05 +0100

    Updated ConsoleLogger declaration to adjust for changes to ILogger.  - Added virtual destructor that was missing.  - Added virtual void flushBuffer()  - Removed buffer from this implementation as it was moved to ILogger.

--- include/ConsoleLogger.hpp
+++ include/ConsoleLogger.hpp
@@ -13,15 +13,13 @@ namespace logpp {
 
     class ConsoleLogger: public ILogger {
         public:
-            ConsoleLogger(string logName, LogLevel maxLogLevel, bool outputBadLogsToStderr, uint32_t bufferSize, bool flushBufferAfterWrite);
+            ConsoleLogger(string logName, LogLevel maxLogLevel, bool outputBadLogsToStderr, uint32_t bufferSize, bool flushBufferAfterWrite); ///!< Object constructor.
+            virtual ~ConsoleLogger(); ///!< Virtual destructor.
             
-            bool flushBufferAfterWrite() { return this->_flushBufferAfterWrite; }
             bool outputBadLogsToStderr() { return this->_outputBadLogsToStderr; }
             bool outputDebugLogsToStderr() { return this->_outputDebugToStderr; }
 
-            uint32_t getBufferSize() { return this->_bufferSize; }
-
-            virtual void logMessage(LogLevel level, string msg);
+            virtual void flushBuffer();
 
             virtual void debug(string msg, exception* except = nullptr, int32_t line = -1, string func = "");
             virtual void error(string msg, exception* except = nullptr, int32_t line = -1, string func = "");
@@ -31,17 +29,12 @@ namespace logpp {
             virtual void trace(string msg, exception* except = nullptr, int32_t line = -1, string func = "");
             virtual void warning(string msg, exception* except = nullptr, int32_t line = -1, string func = "");
 
-            void setBufferSize(bool bufSize) { this->_bufferSize = bufSize; }
-            void setFlushAfterWrite(bool flushAfterWrite) { this->_flushBufferAfterWrite = flushAfterWrite; }
             void setOutputBadLogsToStderr(bool outputToStderr) { this->_outputBadLogsToStderr = outputToStderr; }
             void setOutputDebugLogsToStderr(bool outputToStderr) { this->_outputDebugToStderr = outputToStderr; }
 
         private:
-            bool _flushBufferAfterWrite;
             bool _outputBadLogsToStderr;
             bool _outputDebugToStderr;
-
-            uint32_t _bufferSize;
     };
 
 }
