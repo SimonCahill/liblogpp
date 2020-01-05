33e31a9c2d755d8a9569163925810bd4bd2afbd6
Simon Cahill <simon@h3lix.de>
2020-01-05 17:18:36 +0100

    - Removed redundant usings  - Fixed constructor call for ILogger  - Added missing documentation for logMessage()  - Implemented logMessage()

--- src/ConsoleLogger.cpp
+++ src/ConsoleLogger.cpp
@@ -17,60 +17,48 @@
 
 namespace logpp {
 
-    using std::cerr;
-    using std::cout;
-    using std::endl;
     using std::iostream;
     using std::ostream;
 
-   ConsoleLogger::ConsoleLogger(string logName, LogLevel maxLogLevel, bool outputBadLogsToStderr, uint32_t bufferSize, bool flushBufferAfterWrite):
-   ILogger(logName, maxLogLevel) {
-       setOutputBadLogsToStderr(outputBadLogsToStderr);
-       setBufferSize(bufferSize);
-       setFlushAfterWrite(flushBufferAfterWrite);
-   }
+    ConsoleLogger::ConsoleLogger(string logName, LogLevel maxLogLevel, bool outputBadLogsToStderr, uint32_t bufferSize, bool flushBufferAfterWrite):
+    ILogger(logName, maxLogLevel, flushBufferAfterWrite, bufferSize) {
+        setOutputBadLogsToStderr(outputBadLogsToStderr);
+    }
 
-   ConsoleLogger::~ConsoleLogger() {}
+    ConsoleLogger::~ConsoleLogger() {}
 
-   void ConsoleLogger::flushBuffer() {
-       // TODO: Implement functionality where bad logs are output to cerr if desired.
-       // This will require overriding logMessage()
-       cout << getLogBuffer().str() << endl;
-   }
+    void ConsoleLogger::flushBuffer() {
+        using std::cout;
+        using std::endl;
 
-   void ConsoleLogger::debug(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Debug;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
+        // TODO: Implement functionality where bad logs are output to cerr if desired.
+        // This will require overriding logMessage()
+        cout << getLogBuffer().str() << endl;
+        getLogBuffer().clear();
+    }
 
-   void ConsoleLogger::error(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Error;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
+    /**
+     * @brief Overridden method; logs messages to the internal buffer or standard error output.
+     * 
+     * @remarks This method is overridden. While the core functionality remains the same, 
+     * the major difference is that "bad" logs are automatically redirected to stderr, instead of 
+     * being buffered.
+     * A "bad" log is any log level >= 4.
+     * 
+     * @param level The log level of the message.
+     * @param msg The message to be output.
+     */
+    void ConsoleLogger::logMessage(LogLevel level, string msg) {
+        using std::cerr;
+        using std::endl;
 
-   void ConsoleLogger::fatal(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Fatal;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
+        if (outputBadLogsToStderr() && isBadLog(level)) {
+            // Bypass log buffer and print directly to stderr.
+            cerr << msg << endl;
+            return;
+        }
 
-   void ConsoleLogger::info(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Info;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
-
-   void ConsoleLogger::ok(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Ok;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
-
-   void ConsoleLogger::trace(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Trace;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
-
-   void ConsoleLogger::warning(string msg, exception* except, int32_t line, string func) {
-       auto level = LogLevel::Warning;
-       logMessage(level, formatLogMessage(msg, level, func, line, except));
-   }
+        ILogger::logMessage(level, msg);
+    }
 
 }
