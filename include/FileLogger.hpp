5d6d2adfb8f33ec681bc69c79ab64460e3a2eb6b
Simon Cahill <simon@h3lix.de>
2021-08-16 22:59:38 +0200

    Refactored Doxygen comments Added missing override tags Added new _maxFileCount class member

--- include/FileLogger.hpp
+++ include/FileLogger.hpp
@@ -20,24 +20,29 @@ namespace logpp {
      */
     class FileLogger: public ILogger {
         public:
-            FileLogger(string logName, LogLevel maxLogLevel, string filename, uint32_t bufferSize, 
-                       uint32_t maxFileSizeInMiB, bool flushBufferAfterWrite, bool createFile = false); //!< object constructor
-            virtual ~FileLogger(); //!< virtual destructor
+            static const uint32_t DEFAULT_MAX_LOG_FILES;
 
-            virtual void logMessage(LogLevel level, string msg);
+            FileLogger(string logName, LogLevel maxLogLevel, string filename, uint32_t bufferSize, 
+                       uint32_t maxFileSizeInMiB, bool flushBufferAfterWrite, bool createFile = false); ///!< object constructor
+            virtual ~FileLogger(); ///!< virtual destructor
+
+            virtual void logMessage(LogLevel level, string msg) override;
+
+            virtual FileLogger& setMaxFileCount(const uint32_t maxFileCount = DEFAULT_MAX_LOG_FILES) { _maxFileCount = maxFileCount; return *this; }
 
         private:
             string _filename;
-            uint8_t _numLogs = 0;
-            uint32_t _maxFileSize; //!< max size of log file in MB
+            uint32_t _numLogs = 0;
+            uint32_t _maxFileSize; ///!< max size of log file in MB
+            uint32_t _maxFileCount; ///!< The maximum amount of files logpp is allowed to create before overwriting the files in a loop
 
             bool fileExists(string filename);
             uint32_t fileSize(string filename);
-            virtual void flushBuffer(); //!< Flushes the underlying buffer.
-            const uint32_t maxFileSizeInMiB() const { return _maxFileSize; } //!< getter for _maxFileSize
-            void maxFileSize(const uint32_t maxFileSize) { _maxFileSize = maxFileSize; } //!< setter for _maxFileSize
+            virtual void flushBuffer() override; ///!< Flushes the underlying buffer.
+            uint32_t maxFileSizeInMiB() const { return _maxFileSize; } ///!< getter for _maxFileSize
+            void maxFileSize(const uint32_t maxFileSize) { _maxFileSize = maxFileSize; } ///!< setter for _maxFileSize
     };
 
 }
 
-#endif // !FILE_LOGGER_HPP
+#endif // FILE_LOGGER_HPP
